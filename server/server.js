9c8ac10533add2f63de6b4ea76600d79cf324fd2
Tejas028 <geriatejas58@gmail.com>
2025-05-31 02:54:01 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -11,50 +11,53 @@ import { Server } from 'socket.io'
 dotenv.config()
 
 const app = express()
-const server=http.createServer(app)
-const port = process.env.PORT || 5000
+const server = http.createServer(app)
+
 
 // Initialize socket.io
-export const io=new Server(server,{
-  cors:{origin:"*"}
+export const io = new Server(server, {
+  cors: { origin: "*" }
 })
 
 // store online users
-export const userSocketMap={}; //{userId:socketId}
+export const userSocketMap = {}; //{userId:socketId}
 
 // Socket.io connection handler
-io.on("connection",(socket)=>{
-  const userId=socket.handshake.query.userId
-  console.log("User connected",userId);
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId
+  console.log("User connected", userId);
 
-  if(userId) userSocketMap[userId]=socket.id
+  if (userId) userSocketMap[userId] = socket.id
 
   // Emit online users to all connected clients
-  io.emit("getOnlineUsers",Object.keys(userSocketMap))
+  io.emit("getOnlineUsers", Object.keys(userSocketMap))
   // console.log(5);
-  
-  socket.on("disconnect",()=>{
-    console.log("User Disconnected",userId);
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", userId);
     delete userSocketMap[userId]
-    io.emit("getOnlineUsers",Object.keys(userSocketMap))
-    
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+
   })
-  
+
 })
 
-app.use(express.json({limit:"4mb"}))
+app.use(express.json({ limit: "4mb" }))
 app.use(cors())
 
 app.get('/api/status', (req, res) => {
   res.send('API Working')
 })
-app.use('/api/auth',userRouter)
-app.use('/api/messages',messageRouter)
+app.use('/api/auth', userRouter)
+app.use('/api/messages', messageRouter)
 
 await connectDB()
 await connectCloudinary()
 
-server.listen(port, () => console.log("Server Started", port))
+if (process.env.NODE_ENV !== "production") {
+  const port = process.env.PORT || 5000
+  server.listen(port, () => console.log("Server Started", port))
+}
 
-
-module.exports = app;
+// Export server for vercel
+export default server
